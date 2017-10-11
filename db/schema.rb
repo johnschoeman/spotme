5f250a9cfe29651eb603f5b1d49a9f5339c6ed77
John Schoeman <johnschoeman1617@gmail.com>
2017-10-11 10:49:20 -0700

    Create allUsers query

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008013721) do
+ActiveRecord::Schema.define(version: 20171011173007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171008013721) do
     t.string "avatar_url"
     t.string "fb_name"
     t.string "fb_id"
+    t.string "email", null: false
     t.index ["fb_id"], name: "index_users_on_fb_id", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
