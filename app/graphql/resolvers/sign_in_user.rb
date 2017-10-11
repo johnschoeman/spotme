8f25968ba6dd7b5b66a604c28882673362919457
John Schoeman <johnschoeman1617@gmail.com>
2017-10-11 11:33:21 -0700

    Add signin mutation with basic auth
    NOTE: DO NOT ACTUALLY USE THIS FOR AUTH
    THIS IS JUST PROF OF CONCEPT AND IS ACCEPTABLE FOR PRODUCT

---
+++ app/graphql/resolvers/sign_in_user.rb
@@ -0,0 +1,31 @@
+class Resolvers::SignInUser < GraphQL::Function
+  argument :email, !Types::AuthProviderEmailInput
+  
+  type do
+    name 'SigninPayload'
+    
+    field :token, types.String
+    field :user, Types::UserType
+  end
+
+  def call(_obj, args, ctx)
+    input = args[:email]
+
+    returns unless input
+
+    user = User.find_by email: input[:email]
+
+    return unless user
+    return unless user.authenticate(input[:password])
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
+    token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+
+    ctx[:session][:token] = token
+
+    OpenStruct.new({
+      user: user,
+      token: token
+    })
+  end
+end
--- docs/graphql-server.md
+++ docs/graphql-server.md
@@ -2,4 +2,14 @@
 
 ### Questions
 
+what does 
+```ruby
+has_secure_password
+```
+in the user model do and where does it come from?
+
+----
+
 ### Bugs
+
+Do not forget to set the context of the gql server in the graphql controller.
