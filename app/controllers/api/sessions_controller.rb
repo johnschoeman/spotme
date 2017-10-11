dad33c9eba2b355e856238a90983a8c1edadd3da
John Schoeman <johnschoeman1617@gmail.com>
2017-10-11 13:03:21 -0700

    Add error handling for user resolver

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,19 +1,19 @@
 class Api::SessionsController < ApplicationController
-  # def create
-  #   @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-  #   if @user
-  #     login(@user)
-  #     render "api/users/show"
-  #   else
-  #     render json: ["Invalid username/password"], status: 401
-  #   end
-  # end
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid username/password"], status: 401
+    end
+  end
 
-  # def destroy
-  #   if current_user
-  #     logout
-  #   else
-  #     render json: ["Not Logged In"], status: 404
-  #   end
-  # end
+  def destroy
+    if current_user
+      logout
+    else
+      render json: ["Not Logged In"], status: 404
+    end
+  end
 end
