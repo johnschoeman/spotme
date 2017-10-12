6a053f0815f77722c8257fb02256e416c0eb61e9
John Schoeman <johnschoeman1617@gmail.com>
2017-10-11 20:58:25 -0700

    Remove restful api controllers and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,3 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-
-  helper_method :current_user, :logged_in?, :require_login
-
-  def current_user
-    @user ||= User.find_by_session_token(session[:session_token])
-  end
-
-  def login(user)
-    session[:session_token] = user.reset_session_token!
-  end
-
-  def logout
-    current_user.reset_session_token!
-    session[:session_token] = nil
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
-  def require_login
-    redirect_to new_session_url unless logged_in?
-  end
 end
