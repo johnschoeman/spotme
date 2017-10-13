38a6f7954b9ee19fd7f640608dbcc54fd1477743
Eric Tung <eric.p.tung@gmail.com>
2017-10-13 15:10:21 -0700

    Update create reservation resolver

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -13,7 +13,17 @@
 
 class Reservation < ApplicationRecord
   validates :start_time, :end_time, :user_id, :spot_id, presence: true
+  validate :start_time_must_come_before_end_time
+  # validate :does_not_overlap_existing_reservation
 
   belongs_to :user 
   belongs_to :spot
+
+  def start_time_must_come_before_end_time
+    errors[:start_time] << 'must specify a start time' unless start_time
+    errors[:end_time] << 'must specify an end time' unless end_time
+    errors[:start_time] << 'must come before end time' if start_time > end_time
+  end
+
+
 end
