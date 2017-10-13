18e07c844b6914f37fd8e15d8815960ded410f5f
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-10-13 10:27:25 -0700

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_id = []
+5.times do 
+  User.create(email: Faker::Internet.email, password: 'password')
+  user_id.push(User.last.id)
+end 
+
+until Spot.all.length == 50 do
+  latitude = "37.#{(781730..782836).to_a.sample}".to_f
+  longitude = "-122.#{(491027..495225).to_a.sample}".to_f
+  Spot.create(latitude: latitude, longitude: longitude, user_id: user_id.sample)
+end 
