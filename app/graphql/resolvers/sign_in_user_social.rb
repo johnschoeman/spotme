8236979255c7412fd2f3547ab94f0781595807f6
John Schoeman <johnschoeman1617@gmail.com>
2017-10-12 21:59:14 -0700

    Start social user create and signup

---
+++ app/graphql/resolvers/sign_in_user_social.rb
@@ -0,0 +1,29 @@
+class Resolvers::SocialSignin < GraphQL::Function
+  argument :email, !types.string
+  
+  type do
+    name 'SigninPayload'
+    
+    field :token, types.String
+    field :user, Types::UserType
+  end
+
+  def call(_obj, args, ctx)
+    input = args[:email]
+    returns unless input
+
+    user = User.find_by email: input[:email]
+
+    return unless user
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
+    token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+
+    ctx[:session][:token] = token
+
+    OpenStruct.new({
+      user: user,
+      token: token
+    })
+  end
+end
