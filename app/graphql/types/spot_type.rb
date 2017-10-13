2cb745c89adc394625433f31cb7a7eea72ff1d35
drstonebraker <drew.stonebraker@gmail.com>
2017-10-13 11:10:18 -0700

    Change host_id type to match user id type

--- app/graphql/types/spot_type.rb
+++ app/graphql/types/spot_type.rb
@@ -16,5 +16,5 @@ Types::SpotType = GraphQL::ObjectType.define do
   field :address_zip, types.String, property: :zip
 
   field :hostedBy, -> { Types::UserType }, property: :user
-  field :host_id, types.Int, property: :user_id
+  field :host_id, types.ID, property: :user_id
 end
