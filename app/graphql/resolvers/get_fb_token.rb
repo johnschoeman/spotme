509ca33cda9c4c5af8900a78cce417b9a77d3525
John Schoeman <johnschoeman1617@gmail.com>
2017-10-12 19:59:27 -0700

    Create getfbtoken resolver with fbtokentype

--- app/graphql/resolvers/get_fb_token.rb
+++ app/graphql/resolvers/get_fb_token.rb
@@ -1,15 +1,24 @@
+class FBResponse 
+  attr_accessor :id_token
+end
+
 class Resolvers::GetFBToken < GraphQL::Function
   argument :authorization_code, !types.String
+  argument :redirect_uri, !types.String
 
-  type types.String
+  type Types::FBTokenType
 
-  
   def call(_obj, args, _ctx)
-    id_token = request_id_token(args[:authorization_code])
+    response = request_id_token(args[:authorization_code], args[:redirect_uri])
+    id_token = JSON.parse(response.body)["id_token"]
+    fb_response = FBResponse.new
+    fb_response.id_token = id_token
+    debugger
+    fb_response
   end
 
-  def request_id_token(authorization_code)
-    url = URI("https://YOUR_AUTH0_DOMAIN/oauth/token")
+  def request_id_token(authorization_code, redirect_uri)
+    url = URI("https://spotme.auth0.com/oauth/token")
     
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -17,13 +26,10 @@ class Resolvers::GetFBToken < GraphQL::Function
     
     request = Net::HTTP::Post.new(url)
     request["content-type"] = 'application/json'
-    request.body = "{\"grant_type\":\"authorization_code\",
-                      \"client_id\": \"#{ENV["AUTH0_CLIENT_ID"]}\",
-                      \"client_secret\": \"#{ENV["AUTH0_CLIENT_SECRET"]}\",
-                      \"code\": \"#{authorization_code}\",
-                      \"redirect_uri\": \"https://YOUR_APP/callback\"}"
+    request.body = "{\"grant_type\":\"authorization_code\",\"client_id\": \"#{ENV["AUTH0_CLIENT_ID"]}\",\"client_secret\": \"#{ENV["AUTH0_CLIENT_SECRET"]}\",\"code\": \"#{authorization_code}\",\"redirect_uri\": \"#{redirect_uri}\"}"
     
     response = http.request(request)
-    console.log(response.read_body)
+    debugger
+    response
   end
 end
--- docs/graphql-server.md
+++ docs/graphql-server.md
@@ -43,3 +43,48 @@ NoMethodError (undefined method `id' for true:TrueClass):
 app/controllers/graphql_controller.rb:10:in `execute'
 
 ---
+
+Defining Types is hard.
+in your resolvers you must return a ruby object whose attributes match the return type of the resolver instance as dictated by 'type'
+you may need to make your own class to accomplish this.
+
+```ruby
+
+class FBResponse 
+  attr_accessor :id_token
+end
+
+class Resolvers::GetFBToken < GraphQL::Function
+  argument :authorization_code, !types.String
+  argument :redirect_uri, !types.String
+
+  type Types::FBTokenType
+
+  def call(_obj, args, _ctx)
+    response = request_id_token(args[:authorization_code], args[:redirect_uri])
+    id_token = JSON.parse(response.body)["id_token"]
+    fb_response = FBResponse.new
+    fb_response.id_token = id_token
+    debugger
+    fb_response
+  end
+
+  def request_id_token(authorization_code, redirect_uri)
+    url = URI("https://spotme.auth0.com/oauth/token")
+    
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    request = Net::HTTP::Post.new(url)
+    request["content-type"] = 'application/json'
+    request.body = "{\"grant_type\":\"authorization_code\",\"client_id\": \"#{ENV["AUTH0_CLIENT_ID"]}\",\"client_secret\": \"#{ENV["AUTH0_CLIENT_SECRET"]}\",\"code\": \"#{authorization_code}\",\"redirect_uri\": \"#{redirect_uri}\"}"
+    
+    response = http.request(request)
+    debugger
+    response
+  end
+end
+```
+
+----
