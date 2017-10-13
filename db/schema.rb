676510da34739040275386c1a531ea823112bbca
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-10-13 11:04:37 -0700

    Add reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012181359) do
+ActiveRecord::Schema.define(version: 20171013175535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.float "duration", default: 3600000.0, null: false
+    t.integer "user_id", null: false
+    t.integer "spot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_reservations_on_spot_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "spots", force: :cascade do |t|
     t.float "latitude", null: false
