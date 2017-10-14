2d91a2e2932024dc4f2a1fdc57caae1f1299ecc6
johnschoeman <johnschoeman1617@gmail.com>
2017-10-13 17:07:27 -0700

    assign spot to current user

--- app/graphql/resolvers/create_reservation.rb
+++ app/graphql/resolvers/create_reservation.rb
@@ -6,23 +6,13 @@ class Resolvers::CreateReservation < GraphQL::Function
   type Types::ReservationType
 
   def call(obj, args, ctx)
-    user = User.first
     Reservation.create!(
       start_time: args[:start_time],
       end_time: args[:end_time],
       spot_id: args[:spot_id],
-      user: user
+      user: ctx[:current_user]
       )
   rescue ActiveRecord::RecordInvalid => e
     GraphQL::ExecutionError.new("Invalid input: #{e.record.full_messages.join(', ')}")
   end
 end
-  
-  # user: ctx[:current_user],
-
-# address: {
-#   +    //       street: '1725 Tehama St',
-#   +    //       city: 'San Francisco',
-#   +    //       state: 'CA',
-#   +    //       zip: '94112'
-#   +    //     }
