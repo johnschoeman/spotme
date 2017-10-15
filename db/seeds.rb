d7b80a264dd42b6a229c34ef24a4d6472aa1e393
John Schoeman <johnschoeman1617@gmail.com>
2017-10-15 15:18:36 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'http'
 
 Spot.destroy_all
 User.destroy_all
@@ -14,17 +15,102 @@ user_id = []
   user_id.push(user.id)
 end 
 
-s = Spot.new
-s.id = 1
-s.latitude = 37.7857
-s.longitude = -122.4056
-s.price = 2.0
-s.rating = 5.0
-s.user_id = user_id.sample
-s.save
+# 20.times do
+#   latitude = "37.#{(774929..800500).to_a.sample}".to_f
+#   longitude = "-122.#{(399999..490027).to_a.sample}".to_f
+#   Spot.create(latitude: latitude, 
+#               longitude: longitude, 
+#               user_id: user_id.sample, 
+              # price: [1.50, 2.00, 2.50, 3.00].sample, 
+              # rating: [3.50, 4.00, 4.50, 5.00].sample)
+# end 
 
-20.times do
-  latitude = "37.#{(774929..800500).to_a.sample}".to_f
-  longitude = "-122.#{(399999..490027).to_a.sample}".to_f
-  Spot.create(latitude: latitude, longitude: longitude, user_id: user_id.sample, price: [1.50, 2.00, 2.50, 3.00].sample, rating: [3.50, 4.00, 4.50, 5.00].sample)
-end 
+addresses = [
+  "825 battery st, sf, ca",
+  "160 spear st, sf, ca",
+  "425 tehama st, sf, ca",
+  "600 Montgomery St, sf, ca, 94111",
+  "916 kearny st, sf, ca, 94133",
+  "1 telegraph hill blvd, sf, ca, 94133",
+  "1 Carlton B Goodlett Pl, sf, ca, 94102",
+  "3301 Lyon St, sf, ca, 94123",
+  "50 Hagiwara Tea Garden Dr, sf, ca, 94118",
+  "400 Steiner St, SF, CA, 94117",
+  "1 Ferry Building, sf, ca, 94111",
+  "235 Montgomery st, sf, ca, 94104",
+  "1111 Gough St, sf, ca, 94109",
+  "2500 Steiner st, sf, ca, 94115",
+  "760 Marker St, sf, ca, 94102",
+  "555 California st, sf, ca, 94104",
+  "140 new montgomery, sf ca",
+  "999 california st, sf, ca, 94108",
+  "870 market st, sf, ca, 94102",
+  "1100 california st, sf, ca, 94108",
+  "582 market st, sf, ca, 94104",
+  "3321 16th st, sf, ca, 94114",
+  "2 new montgomery st, sf, ca, 94105"
+]
+
+image_url = [
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105088/parking_spots/reserved.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105130/parking_spots/images.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105245/parking_spots/Residential-Driveway.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105258/parking_spots/CobblestonePaverDriveway-GettyImages-83116815-58f639a85f9b581d59e6d03b.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105270/parking_spots/Driveways-Concrete.jpg.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105287/parking_spots/20101201_ExposedDriveway.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105299/parking_spots/cc7a911af7587bec74fefd1efd3b9849--gravel-driveway-diy-driveway.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105318/parking_spots/BrookfieldHouse_023-e1423451943273-320x252.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105330/parking_spots/6532b4d8e06e9ae0bd5c490b748a1368--driveway-landscaping-driveway-design.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105349/parking_spots/043a654887548a0016ed7d4cc39c2665.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105379/parking_spots/parking-spot-from-above-with-spots.jpg",
+  "https://res.cloudinary.com/dekgrddbo/image/upload/v1508105401/parking_spots/2015-03-27-19.39.37.jpg"
+]
+
+def create_spot(args, user_id, image_url)
+  address_response = request_address(args[:address])
+  address_hash = parse_address_response(address_response)
+  Spot.create!(
+    latitude: address_hash[:latitude],
+    longitude: address_hash[:longitude],
+    title: args[:title],
+    image_url: image_url.sample,
+    rating: [3.50, 4.00, 4.50, 5.00].sample,
+    price: [1.50, 2.00, 2.50, 3.00].sample,
+    description: args[:description],
+    number: address_hash[:number],
+    street: address_hash[:street],
+    city: address_hash[:city],
+    state: address_hash[:state],
+    zip: address_hash[:zip],
+    user_id: user_id.sample
+    )
+  end
+
+  def request_address(address)
+    address = address.gsub(/\s+/, '+')
+    HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV['GOOGLE_MAPS_API_KEY']}")
+  end
+
+  def parse_address_response(address_response)
+    address_hash = {}
+    parsed_response = address_response.parse["results"][0]
+    address_hash[:latitude] = parsed_response["geometry"]["location"]["lat"]
+    address_hash[:longitude] = parsed_response["geometry"]["location"]["lng"]
+    address_hash[:number] = get_address_component(parsed_response["address_components"], "street_number")
+    address_hash[:street] = get_address_component(parsed_response["address_components"], "route")
+    address_hash[:city] = get_address_component(parsed_response["address_components"], "locality")
+    address_hash[:state] = get_address_component(parsed_response["address_components"], "administrative_area_level_1")
+    address_hash[:zip] = get_address_component(parsed_response["address_components"], "postal_code")
+    address_hash
+  end
+
+  def get_address_component(components, type)
+    component = components.find do |component|
+      component["types"].include?(type)
+    end
+    type == 'locality' ? component['long_name'] : component["short_name"]
+  end
+
+  addresses.each do |address|
+    create_spot({address: address}, user_id, image_url)
+  end
